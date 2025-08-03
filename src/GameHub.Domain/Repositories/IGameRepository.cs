917f2c5fe5842133014fa1dbf2a2aad59321f0a0
Gustavo Rodrigues <gusropdev@hotmail.com>
2025-08-03 14:29:45 -0300

    feat: modifica GetByGenre e GeyByPlatform para funcionarem devidamente com a atualização das propriedades para Enum

--- src/GameHub.Domain/Repositories/IGameRepository.cs
+++ src/GameHub.Domain/Repositories/IGameRepository.cs
@@ -5,13 +5,13 @@ namespace GameHub.Domain.Repositories;
 
 public interface IGameRepository
 {
-    Task<Game> GetByIdAsync(Guid gameId, CancellationToken cancellationToken = default);
+    Task<Game?> GetByIdAsync(Guid gameId, CancellationToken cancellationToken = default);
     Task<List<Game>> GetAllAsync(CancellationToken cancellationToken = default);
     Task<List<Game>> GetActiveAsync(CancellationToken cancellationToken = default);
-    Task<List<Game>> GetByGenreAsync(string genre, CancellationToken cancellationToken = default);
-    Task<List<Game>> GetByPlatformAsync(string platform, CancellationToken cancellationToken = default);
+    Task<List<Game>> GetInactiveAsync(CancellationToken cancellationToken = default);
     Task<List<Game>> GetByAgeRatingAsync(AgeRating ageRating, CancellationToken cancellationToken = default);
+    Task<List<Game>> GetByGenreAsync(Genre genre, CancellationToken cancellationToken = default);
+    Task<List<Game>> GetByPlatformAsync(Platform platform, CancellationToken cancellationToken = default);
     Task AddAsync(Game game, CancellationToken cancellationToken = default);
-    Task UpdateAsync(Game game, CancellationToken cancellationToken = default);
-    Task DeleteAsync(Game game, CancellationToken cancellationToken = default);
+    void Delete(Game game, CancellationToken cancellationToken = default);
 }

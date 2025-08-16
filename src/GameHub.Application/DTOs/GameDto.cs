81224a603e33e62b1f80136b18678dd4cc9e6be7
Gustavo Rodrigues <gusropdev@hotmail.com>
2025-08-16 17:47:13 -0300

    fix: arruma GameDTO

--- src/GameHub.Application/DTOs/GameDto.cs
+++ src/GameHub.Application/DTOs/GameDto.cs
@@ -5,8 +5,8 @@ namespace GameHub.Application.DTOs;
 public class GameDto
 {
     public Guid Id { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
     public decimal DailyRentalPrice { get; set; }
     public int StockQuantity { get; set; }
     public DateTime ReleaseDate { get; set; }

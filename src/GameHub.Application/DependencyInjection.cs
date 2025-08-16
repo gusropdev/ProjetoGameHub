15e29bd4cc16472c9314f1d265efd5be819b9b87
Gustavo Rodrigues <gusropdev@hotmail.com>
2025-08-16 17:47:47 -0300

    feat: adiciona injeção de dependência do FluentValidation dentro do Application

--- src/GameHub.Application/DependencyInjection.cs
+++ src/GameHub.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GameHub.Application;
@@ -8,6 +9,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
         return services;
     }
